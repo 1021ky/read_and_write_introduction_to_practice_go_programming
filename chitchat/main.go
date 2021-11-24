9fd8fde81c52bae23a85d87174cf8433cd1c4194
1021ky <2500024+1021ky@users.noreply.github.com>
2021-11-24 19:22:15 +0900

    ハンドラのメイン処理の雛形を追加

---
+++ chitchat/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// マルチプレクサを生成、リクエストをハンドラにリダイレクト
+	mux := http.NewServeMux()
+
+	// 静的ファイルの返送を設定
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// ルートURLをハンドラ関数にリダイレクトする
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+}
