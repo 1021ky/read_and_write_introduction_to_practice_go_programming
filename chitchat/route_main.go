fe36c3387c2da02e6831f437f59c3669dab414ae
1021ky <2500024+1021ky@users.noreply.github.com>
2021-11-27 15:54:31 +0900

    ビルドできるように一度コメントアウト

--- chitchat/route_main.go
+++ chitchat/route_main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"net/http"
-	"github.com/1021ky/read_and_write_introduction_to_practice_go_programming/chitchat/data"
+	// "github.com/1021ky/read_and_write_introduction_to_practice_go_programming/chitchat/data"
 )
 
 // HTTPハンドラ関数
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-	templates := template.Must(template.ParseFiles(files...))
-	threads,  err := data.Threads(); if err == nil{
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
+	// files := []string{
+	// 	"templates/layout.html",
+	// 	"templates/navbar.html",
+	// 	"templates/index.html",
+	// }
+	// templates := template.Must(template.ParseFiles(files...))
+	// threads,  err := data.Threads(); if err == nil{
+	// 	templates.ExecuteTemplate(w, "layout", threads)
+	// }
 }
